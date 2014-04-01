8d2783e41bbb428c95ef290143440df5102633b8
Tyler H <tyhoffman1@gmail.com>
2014-04-01 18:52:21 -0400

    Initial Commit

---
+++ FreeGeoIP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FreeGeoIP
+//
+//  Created by Tyler H on 4/1/14.
+//  Copyright (c) 2014 Tyler Hoffman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
